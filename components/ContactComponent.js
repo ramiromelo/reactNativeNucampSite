45697aae53fa2daaf851db49a745ba82455419ef
Ramiro Melo <ramiromelo@gmail.com>
2021-03-19 11:47:44 -0700

    Animatable

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Text } from 'react-native';
 import { Card } from 'react-native-elements';
 import { ScrollView } from 'react-native';
+import * as Animatable from 'react-native-animatable';
 
 class Contact extends Component {
    constructor(props) {
@@ -18,22 +19,24 @@ class Contact extends Component {
   render() {
      return (
          <ScrollView>
-            <Card 
-                title="Contact Information"
-                wrapperStyle={{margin: 20}}
-            >
-                <Text style={{marginBottom: 10}}>
-                    1 Nucamp Way{"\n"}
-                    Seattle, WA 98001{"\n"}
-                    U.S.A
-                </Text>
-                <Text style={{marginBottom: 10}}>
-                    Phone: 1-206-555-1234{"\n"}
-                    Email: campsites@nucamp.co
-                </Text>
-                
+            <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+                <Card 
+                    title="Contact Information"
+                    wrapperStyle={{margin: 20}}
+                >
+                    <Text style={{marginBottom: 10}}>
+                        1 Nucamp Way{"\n"}
+                        Seattle, WA 98001{"\n"}
+                        U.S.A
+                    </Text>
+                    <Text style={{marginBottom: 10}}>
+                        Phone: 1-206-555-1234{"\n"}
+                        Email: campsites@nucamp.co
+                    </Text>
+                    
 
-            </Card>
+                </Card>
+            </Animatable.View>
          </ScrollView>
          
      )
--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -5,6 +5,7 @@ import { Tile } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
 import Loading from './LoadingComponent';
+import * as Animatable from 'react-native-animatable';
 
 const mapStateToProps = state => {
     return {
@@ -24,13 +25,16 @@ class Directory extends Component {
 
         const renderDirectoryItem = ({item}) => {
             return (
-                <Tile
-                    title={item.name}
-                    caption={item.description}
-                    featured
-                    onPress={() => navigate('CampsiteInfo', { campsiteId: item.id })}
-                    imageSrc={{uri: baseUrl + item.image}}
-                />
+                <Animatable.View animation='fadeInRightBig' duration={2000}>
+                    <Tile
+                        title={item.name}
+                        caption={item.description}
+                        featured
+                        onPress={() => navigate('CampsiteInfo', { campsiteId: item.id })}
+                        imageSrc={{uri: baseUrl + item.image}}
+                    />
+                </Animatable.View>
+
             );
         };
         
